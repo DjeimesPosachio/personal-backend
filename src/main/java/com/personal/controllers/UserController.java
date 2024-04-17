7e401736728404053ae97586efaba30ed958f59a
lordeiviny <132486172+lordeiviny@users.noreply.github.com>
2024-04-17 10:19:29 -0300

    fix:usercontroller

--- src/main/java/com/personal/controllers/UserController.java
+++ src/main/java/com/personal/controllers/UserController.java
@@ -1,30 +1,38 @@
 package com.personal.controllers;
 
+import com.personal.dtos.request.UserRequestDto;
+import com.personal.dtos.response.UserResponseDto;
+
 import com.personal.entities.User;
 import com.personal.repositories.IUserRepository;
-import org.aspectj.apache.bcel.Repository;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Usuario", description = "Endpoints de exemplo")
 @RestController
-@RequestMapping (value = "user")
-
+@RequestMapping("user")
 public class UserController {
+
     @Autowired
-    IUserRepository repository;
+    private IUserRepository repository;
 
-    @GetMapping
-    public List <User> getAll() {
-        return repository.findAll();
-
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping
+    public ResponseEntity<UserResponseDto> create (@RequestBody UserRequestDto dto) {
+        User user = repository.save(new User(dto));
+        return ResponseEntity.ok(new UserResponseDto(user));
     }
 
-    @PostMapping
-    public void  create(@RequestBody User user) {
-        repository.save(user);
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping
+    public List<UserResponseDto> GetAll() {
+        return repository.findAll().stream().map(UserResponseDto::new).toList();
     }
 
 }
-
