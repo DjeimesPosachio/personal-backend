3c76899f49a973c1a76652ffe0b6f7d043a5bf97
Djeimes Posachio <djeimes_lyncon@hotmail.com>
2024-06-03 20:47:46 -0300

    Remoção imports inutilizados

--- src/main/java/com/personal/controllers/UsuarioController.java
+++ src/main/java/com/personal/controllers/UsuarioController.java
@@ -6,7 +6,6 @@ import com.personal.services.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
--- src/main/java/com/personal/services/AlunoService.java
+++ src/main/java/com/personal/services/AlunoService.java
@@ -8,7 +8,6 @@ import com.personal.exceptions.EventNotFoundException;
 import com.personal.repositories.AlunoRepository;
 import com.personal.repositories.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
